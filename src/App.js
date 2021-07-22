13f32a22cfc04aa99600650bccc53becea4c05ab
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-07-22 09:11:45 -0400

    Set up git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import Particles from 'react-particles-js';
+import { Component } from 'react';
+import Clarifai from 'clarifai';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: '04b99e8a9dec4791aa70fd8b2a9f7bda'
+});
+
+const particlesSetting = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      },
+    }
+  }
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input })
+    app.models
+      .predict(
+        'c0c0ac362b03416da06ab3fa36fb58e3',
+        this.state.input)
+      .then(
+        function (response) {
+          console.log(response);
+        }, function (err) {
+
+        }
+      );
+  }
+  render() {
+    return (
+      <div className="App" >
+        <Particles className='particles' params={particlesSetting} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
