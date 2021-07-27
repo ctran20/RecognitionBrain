da077c020107e75a8f3f87442318e1d658ecb2de
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-07-27 13:36:17 -0400

    Added server codes

---
+++ Backend Server/server.js
@@ -0,0 +1,86 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import cors from 'cors';
+import knex from 'knex';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.post('/signin', (req, res) => {
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            if (bcrypt.compareSync(req.body.password, data[0].hash)) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))
+            } else {
+                res.status(400).json('wrong email or password');
+            }
+        }).catch(err => res.status(400).json('wrong email or password'))
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+
+    db.select('*').from('users').where({ id })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0]);
+            } else {
+                res.status(400).json('id not found');
+            }
+        })
+        .catch(err => res.status(400).json('error getting user'))
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    const hash = bcrypt.hashSync(password, 10);
+
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        name: name,
+                        email: loginEmail[0],
+                        joined: new Date()
+                    }).then(user => {
+                        res.json(user[0]);
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+    })
+        .catch(err => res.status(400).json('Unable to register!'));
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0])
+        })
+        .catch(err => res.status(400).json('Unable to get entries!'))
+
+})
+
+app.listen(3000, () => {
+    console.log('App is running on port 3000');
+})
